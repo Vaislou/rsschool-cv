99cbf811a5c755e9a499f7e67a9d95a011e87bea
Vaislou <vaislou@gmail.com>
2021-09-16 03:13:37 +0300

    fix: add CSS styles for mobile devices

--- js/script.js
+++ js/script.js
@@ -27,4 +27,23 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         })
     })
+
+    //Works Slider
+
+    let dots = document.querySelectorAll('.works__dot');
+    let worksItems = document.querySelectorAll('.works__item')
+
+    dots.forEach(function (elem, index) {
+        elem.addEventListener('click', function() {
+            if (!elem.classList.contains('works__dot_active')) {
+                dots.forEach(elem => elem.classList.remove('works__dot_active'));
+                elem.classList.add('works__dot_active');
+                worksItems.forEach(e => e.classList.remove('works__item_active'));
+                let count = index * 4;
+                for (let i = count; i < count + 4; i++) {
+                    worksItems[i].classList.add('works__item_active');
+                }
+            }
+        })
+    })
 })
